19076cd46a1378e89c1a67d836d9db8c0d0f726d
sahilkhandekar2801 <sahilkhandekar5695@gmail.com>
2024-07-20 12:18:38 +0530

    try

---
+++ lecture 3.py
@@ -0,0 +1,12 @@
+#  LIST AND TUPPLES 
+# list are created using square brackets.
+# Lists are used to store multiple items in a single variable.
+# lets take an example if want to store a marks of many students.
+# we can store any datatype in list for ex(string,float,int,etc)
+# marks = [88.8,89.9,79.8,99.9]      
+# list is written using [] and set is written using {}
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[3])
+# print(len(marks))
